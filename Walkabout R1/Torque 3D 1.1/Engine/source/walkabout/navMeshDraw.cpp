a3fd87713f237b93409dfab6de7c1813fe4bbd03
Daniel Buckmaster <dan.buckmaster@gmail.com>
2012-07-16 08:49:40 +0000

    Added progress so far.

---
+++ Walkabout R1/Torque 3D 1.1/Engine/source/walkabout/navMeshDraw.cpp
@@ -0,0 +1,130 @@
+//
+// T3D 1.1 implementation of Recast's DebugUtils.
+// Daniel Buckmaster, 2011
+//
+
+#include "navMeshDraw.h"
+
+#include "gfx/gfxDevice.h"
+#include "gfx/primBuilder.h"
+#include "gfx/gfxStateBlock.h"
+
+namespace Nav {
+
+   duDebugDrawTorque::duDebugDrawTorque()
+   {
+      mOverrideColour = false;
+      mOverrideAlpha = false;
+      mColour = 0;
+      mAlpha = 0;
+   }
+
+   duDebugDrawTorque::~duDebugDrawTorque()
+   {
+   }
+
+   void duDebugDrawTorque::depthMask(bool state)
+   {
+      mDesc.setZReadWrite(true, state);
+   }
+
+   void duDebugDrawTorque::texture(bool state)
+   {
+   }
+
+   void duDebugDrawTorque::begin(duDebugDrawPrimitives prim, float size)
+   {
+      mQuadsMode = false;
+      mVertCount = 0;
+      mPrimType = 0;
+      switch(prim)
+      {
+      case DU_DRAW_POINTS: mPrimType = GFXPointList; break;
+      case DU_DRAW_LINES:  mPrimType = GFXLineList; break;
+      case DU_DRAW_TRIS:   mPrimType = GFXTriangleList; break;
+      case DU_DRAW_QUADS:  mPrimType = GFXTriangleList; mQuadsMode = true; break;
+      }
+      mDesc.setCullMode(GFXCullNone);
+      mDesc.setBlend(true);
+      GFXStateBlockRef sb = GFX->createStateBlock(mDesc);
+      GFX->setStateBlock(sb);
+      PrimBuild::begin((GFXPrimitiveType)mPrimType, 128);
+   }
+
+   void duDebugDrawTorque::vertex(const float* pos, unsigned int color)
+   {
+      vertex(pos[0], pos[1], pos[2], color);
+   }
+
+   void duDebugDrawTorque::vertex(const float x, const float y, const float z, unsigned int color)
+   {
+      if(mOverrideColour)
+         color = mColour;
+      U8 r, g, b, a;
+      rcCol(color, r, g, b, a);
+
+      if(mOverrideAlpha)
+         a = mAlpha;
+
+      PrimBuild::color4i(r, g, b, a);
+
+      if(mQuadsMode)
+      {
+         mVertCount++;
+         // Handle quads
+         switch(mVertCount)
+         {
+         case 1:
+            mStore[0][0] = x;
+            mStore[0][1] = -z;
+            mStore[0][2] = y;
+            break;
+         case 3:
+            mStore[1][0] = x;
+            mStore[1][1] = -z;
+            mStore[1][2] = y;
+            break;
+         case 4:
+            PrimBuild::vertex3f(x, -z, y);
+            PrimBuild::vertex3f(mStore[0][0], mStore[0][1], mStore[0][2]);
+            PrimBuild::vertex3f(mStore[1][0], mStore[1][1], mStore[1][2]);
+            mVertCount = 0;
+            break;
+         }
+      }
+      PrimBuild::vertex3f(x, -z, y);
+   }
+
+   void duDebugDrawTorque::vertex(const float* pos, unsigned int color, const float* uv)
+   {
+      vertex(pos[0], pos[1], pos[2], color);
+   }
+
+   void duDebugDrawTorque::vertex(const float x, const float y, const float z, unsigned int color, const float u, const float v)
+   {
+      vertex(x, y, z, color);
+   }
+
+   void duDebugDrawTorque::end()
+   {
+      PrimBuild::end();
+   }
+
+   void duDebugDrawTorque::overrideColour(unsigned int col)
+   {
+      mOverrideColour = true;
+      mColour = col;
+   }
+
+   void duDebugDrawTorque::overrideAlpha(unsigned char alpha)
+   {
+      mOverrideAlpha = true;
+      mAlpha = alpha;
+   }
+
+   void duDebugDrawTorque::cancelOverride()
+   {
+      mOverrideColour = false;
+      mOverrideAlpha = false;
+   }
+};
---
+++ Walkabout R1/Torque 3D 1.2/game/tools/navEditor/navEditorGui.cs
@@ -0,0 +1,308 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) Daniel Buckmaster 2012
+//-----------------------------------------------------------------------------
+
+$Nav::EditorOpen = false;
+
+function NavEditorGui::onEditorActivated(%this)
+{
+   // When the editor is activated, the last NavMesh selected remains so. All
+   // other objects are deselected.
+   %count = EWorldEditor.getSelectionSize();
+   %nm = "";
+   while(%count)
+   {
+      %obj = EWorldEditor.getSelectedObject(0);
+      if(%obj.getClassName() $= "NavMesh")
+         %nm = %obj;
+      EWorldEditor.unselectObject(%obj);
+      %count--;
+   }
+   if(isObject(%nm))
+      %this.selectObject(%nm);
+   %this.prepSelectionMode();
+}
+
+function NavEditorGui::onEditorDeactivated(%this)
+{
+}
+
+function NavEditorGui::onModeSet(%this, %mode)
+{
+   // Callback when the nav editor changes mode. Set the appropriate dynamic
+   // GUI contents in the properties/actions boxes.
+   NavInspector.setVisible(false);
+
+   %actions = NavEditorOptionsWindow->ActionsBox;
+   %actions->SelectActions.setVisible(false);
+   %actions->LinkActions.setVisible(false);
+   %actions->CoverActions.setVisible(false);
+   %actions->TileActions.setVisible(false);
+   %actions->TestActions.setVisible(false);
+
+   %properties = NavEditorOptionsWindow->PropertiesBox;
+   %properties->LinkProperties.setVisible(false);
+   %properties->TileProperties.setVisible(false);
+   %properties->TestProperties.setVisible(false);
+
+   switch$(%mode)
+   {
+   case "SelectMode":
+      NavInspector.setVisible(true);
+      %actions->SelectActions.setVisible(true);
+   case "LinkMode":
+      %actions->LinkActions.setVisible(true);
+      %properties->LinkProperties.setVisible(true);
+   case "CoverMode":
+      // 
+      %actions->CoverActions.setVisible(true);
+   case "TileMode":
+      %actions->TileActions.setVisible(true);
+      %properties->TileProperties.setVisible(true);
+   case "TestMode":
+      %actions->TestActions.setVisible(true);
+      %properties->TestProperties.setVisible(true);
+   }
+}
+
+function NavEditorGui::paletteSync(%this, %mode)
+{
+   // Synchronise the palette (small buttons on the left) with the actual mode
+   // the nav editor is in.
+   %evalShortcut = "ToolsPaletteArray-->" @ %mode @ ".setStateOn(1);";
+   eval(%evalShortcut);
+} 
+
+function NavEditorGui::onEscapePressed(%this)
+{
+   return false;
+}
+
+function NavEditorGui::selectObject(%this, %obj)
+{
+   NavTreeView.clearSelection();
+   if(isObject(%obj))
+      NavTreeView.selectItem(%obj);
+   %this.onObjectSelected(%obj);
+}
+
+function NavEditorGui::onObjectSelected(%this, %obj)
+{
+   if(%this.selectedObject == %obj)
+      return;
+   if(isObject(%this.selectedObject))
+      %this.deselect();
+   %this.selectedObject = %obj;
+   if(isObject(%obj))
+      %this.selectMesh(%obj);
+}
+
+function NavEditorGui::deleteMesh(%this)
+{
+   if(isObject(%this.selectedObject))
+   {
+      %this.selectedObject.delete();
+      %this.selectObject(-1);
+   }
+}
+
+function NavEditorGui::deleteSelected(%this)
+{
+   switch$(%this.getMode())
+   {
+   case "SelectMode":
+      // Try to delete the selected NavMesh.
+      if(isObject(NavEditorGui.selectedObject))
+         MessageBoxYesNo("Warning",
+            "Are you sure you want to delete this NavMesh?",
+            "NavEditorGui.deleteMesh();");
+   case "TestMode":
+      %this.getPlayer().delete();
+      %this.onPlayerDeselected();
+   case "LinkMode":
+      %this.deleteLink();
+      %this.isDirty = true;
+   }
+}
+
+function NavEditorGui::buildSelectedMeshes(%this)
+{
+   if(isObject(%this.getMesh()))
+   {
+      %this.getMesh().build();
+      %this.isDirty = true;
+   }
+}
+
+function NavEditorGui::buildLinks(%this)
+{
+   if(isObject(%this.getMesh()))
+   {
+      %this.getMesh().buildLinks();
+      %this.isDirty = true;
+   }
+}
+
+function updateLinkData(%control, %flags)
+{
+error(%flags);
+   %control->LinkWalkFlag.setVisible(true);
+   %control->LinkJumpFlag.setVisible(true);
+   %control->LinkDropFlag.setVisible(true);
+   %control->LinkLedgeFlag.setVisible(true);
+   %control->LinkClimbFlag.setVisible(true);
+   %control->LinkTeleportFlag.setVisible(true);
+
+   %control->LinkWalkFlag.setStateOn(%flags & $Nav::WalkFlag);
+   %control->LinkJumpFlag.setStateOn(%flags & $Nav::JumpFlag);
+   %control->LinkDropFlag.setStateOn(%flags & $Nav::DropFlag);
+   %control->LinkLedgeFlag.setStateOn(%flags & $Nav::LedgeFlag);
+   %control->LinkClimbFlag.setStateOn(%flags & $Nav::ClimbFlag);
+   %control->LinkTeleportFlag.setStateOn(%flags & $Nav::TeleportFlag);
+}
+
+function getLinkFlags(%control)
+{
+   return (%control->LinkWalkFlag.isStateOn() ? $Nav::WalkFlag : 0) |
+          (%control->LinkJumpFlag.isStateOn() ? $Nav::JumpFlag : 0) |
+          (%control->LinkDropFlag.isStateOn() ? $Nav::DropFlag : 0) |
+          (%control->LinkLedgeFlag.isStateOn() ? $Nav::LedgeFlag : 0) |
+          (%control->LinkClimbFlag.isStateOn() ? $Nav::ClimbFlag : 0) |
+          (%control->LinkTeleportFlag.isStateOn() ? $Nav::TeleportFlag : 0);
+}
+
+function disableLinkData(%control)
+{
+   %control->LinkWalkFlag.setVisible(false);
+   %control->LinkJumpFlag.setVisible(false);
+   %control->LinkDropFlag.setVisible(false);
+   %control->LinkLedgeFlag.setVisible(false);
+   %control->LinkClimbFlag.setVisible(false);
+   %control->LinkTeleportFlag.setVisible(false);
+}
+
+function NavEditorGui::onLinkSelected(%this, %flags)
+{
+   updateLinkData(NavEditorOptionsWindow-->LinkProperties, %flags);
+}
+
+function NavEditorGui::onPlayerSelected(%this, %flags)
+{
+   updateLinkData(NavEditorOptionsWindow-->TestProperties, %flags);
+}
+
+function NavEditorGui::updateLinkFlags(%this)
+{
+   if(isObject(%this.getMesh()))
+   {
+      %properties = NavEditorOptionsWindow-->LinkProperties;
+      %this.setLinkData(getLinkFlags(%properties));
+      %this.isDirty = true;
+   }
+}
+
+function NavEditorGui::updateTestFlags(%this)
+{
+   if(isObject(%this.getPlayer()))
+   {
+      %properties = NavEditorOptionsWindow-->TestProperties;
+      %player = %this.getPlayer();
+
+      %player.allowWwalk = %properties->LinkWalkFlag.isStateOn();
+      %player.allowJump = %properties->LinkJumpFlag.isStateOn();
+      %player.allowDrop = %properties->LinkDropFlag.isStateOn();
+      %player.allowLedge = %properties->LinkLedgeFlag.isStateOn();
+      %player.allowClimb = %properties->LinkClimbFlag.isStateOn();
+      %player.allowTeleport = %properties->LinkTeleportFlag.isStateOn();
+
+      %this.isDirty = true;
+   }
+}
+
+function NavEditorGui::onLinkDeselected(%this)
+{
+   disableLinkData(NavEditorOptionsWindow-->LinkProperties);
+}
+
+function NavEditorGui::onPlayerDeselected(%this)
+{
+   disableLinkData(NavEditorOptionsWindow-->TestProperties);
+}
+
+function NavEditorGui::createCoverPoints(%this)
+{
+   if(isObject(%this.getMesh()))
+   {
+      %this.getMesh().createCoverPoints();
+      %this.isDirty = true;
+   }
+}
+
+function NavEditorGui::deleteCoverPoints(%this)
+{
+   if(isObject(%this.getMesh()))
+   {
+      %this.getMesh().deleteCoverPoints();
+      %this.isDirty = true;
+   }
+}
+
+function NavInspector::inspect(%this, %obj)
+{
+   %name = "";
+   if(isObject(%obj))
+      %name = %obj.getName();
+   else
+      NavFieldInfoControl.setText("");
+
+   Parent::inspect(%this, %obj);
+}
+
+function NavInspector::onInspectorFieldModified(%this, %object, %fieldName, %arrayIndex, %oldValue, %newValue)
+{
+   // Same work to do as for the regular WorldEditor Inspector.
+   Inspector::onInspectorFieldModified(%this, %object, %fieldName, %arrayIndex, %oldValue, %newValue);
+}
+
+function NavInspector::onFieldSelected(%this, %fieldName, %fieldTypeStr, %fieldDoc)
+{
+   NavFieldInfoControl.setText("<font:ArialBold:14>" @ %fieldName @ "<font:ArialItalic:14> (" @ %fieldTypeStr @ ") " NL "<font:Arial:14>" @ %fieldDoc);
+}
+
+function NavTreeView::onInspect(%this, %obj)
+{
+   NavInspector.inspect(%obj);
+}
+
+function NavTreeView::onSelect(%this, %obj)
+{
+   NavInspector.inspect(%obj);
+   NavEditorGui.onObjectSelected(%obj);
+}
+
+function NavEditorGui::prepSelectionMode(%this)
+{
+   %this.setMode("SelectMode");
+   ToolsPaletteArray-->NavEditorSelectMode.setStateOn(1);
+}
+
+//-----------------------------------------------------------------------------
+
+function ENavEditorPaletteButton::onClick(%this)
+{
+   // When clicking on a pelette button, add its description to the bottom of
+   // the editor window.
+   EditorGuiStatusBar.setInfo(%this.DetailedDesc);
+}
+
+//-----------------------------------------------------------------------------
+
+function NavMeshLinkFlagButton::onClick(%this)
+{
+   NavEditorGui.updateLinkFlags();
+}
+
+function NavMeshTestFlagButton::onClick(%this)
+{
+   NavEditorGui.updateTestFlags();
+}
